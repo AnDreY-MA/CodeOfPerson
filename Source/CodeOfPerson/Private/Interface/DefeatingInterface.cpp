764ab3408add529e9a24867b775b9672923dda24
Mait <andreymamontov9@gmail.com>
2024-11-29 14:59:01 +0300

    Aded animations for Alina after defeat boss

---
+++ Source/CodeOfPerson/Private/Interface/DefeatingInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/DefeatingInterface.h"
+
+// Add default functionality here for any IDefeatingInterface functions that are not pure virtual.
